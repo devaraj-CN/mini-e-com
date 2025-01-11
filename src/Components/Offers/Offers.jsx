c73ad2f2ca84b5bcf5b54f7ae6228e9381e4660a
Deva-CN <devaraj@codenatives.com>
2025-01-11 13:49:04 +0530

    designing collection

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Offers.css'
+import exclusive_image from '../Assets/exclusive_image.png'
 
 const Offers = () => {
     return (
-        <div>Offers</div>
+        <div className='offers'>
+            <div className="offers-left">
+                <h1>Exclusive</h1>
+                <h1>OFfers For You</h1>
+                <p>Only on Best Sellers Products</p>
+            </div>
+            <div className="offers-right">
+                <img src={exclusive_image} alt="exclusive" />
+            </div>
+        </div>
     )
 }
 
