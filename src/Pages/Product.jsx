278b9852a6571fb931232f7ad4abd6acb1333c0a
Deva-CN <devaraj@codenatives.com>
2025-01-17 19:06:13 +0530

    product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
 
 const Product = () => {
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
   return (
-    <div>Product</div>
+    <div>
+      <Breadcrums />
+    </div>
   )
 }
 
-export default Product
+export default Product;
