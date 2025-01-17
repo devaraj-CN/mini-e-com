afa49fc4b5475a5e3aa3c11195411959053f573e
Deva-CN <devaraj@codenatives.com>
2025-01-17 15:57:57 +0530

    login signup

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Your Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <button>Continue</button>
+        </div>
+
+        <p className="loginsignup-login">Already have an Account? <span>Login Here</span> </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            I agree to the Terms of Service and Privacy Policy
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
